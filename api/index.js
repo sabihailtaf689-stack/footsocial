6f0962a88238175dc1a6694d2aa23d7a28155035
Sabih <sabihailtaf689@gmail.com>
2026-01-02 17:40:39 +0500

    Fix Vercel 404 error: update API handler and add static file rewrites

--- api/index.js
+++ api/index.js
@@ -4,4 +4,8 @@ const app = require('../server.js');
 
 // Export handler function for Vercel
 // Vercel will call this function for all /api/* routes
-module.exports = app;
+// The app is already exported from server.js, but we need to ensure it's properly handled
+module.exports = (req, res) => {
+  // Vercel passes the request directly to the Express app
+  return app(req, res);
+};
